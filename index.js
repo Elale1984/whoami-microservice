10fe3bd9b2b6187ec431ea5ec7f233d50663c4d1
elale1984 <edward.gratefuldead1@gmail.com>
2025-01-29 15:21:48 -0600

    finished

--- index.js
+++ index.js
@@ -3,8 +3,22 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
 var cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/api/whoami", (req, res) => {
+  const clientIp = req.ip;
+  const userAgent = req.get("User-Agent");
+  const language = req.get("Accept-Language");
+
+  res.json({ ipaddress: clientIp, language: language, software: userAgent });
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
