b1fc9b56efdec4e52fdc26a26401aabc08523aa2
elale1984 <edward.gratefuldead1@gmail.com>
2025-01-29 14:59:55 -0600

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 }));
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
